6c0f6b4552c8fca6d6970998751fadd7cc2f1d3e
Daniel Beal <dbeal@wiser.com>
2020-11-04 15:56:10 -0800

    add quotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ curl -Lo /usr/bin/argocd https://github.com/argoproj/argo-cd/releases/download/v
 chmod +x /usr/bin/argocd
 
 # TODO: latesr comment directly on the build and block it if fail
-argocd --server $INPUT_SERVER --auth-token $INPUT_TOKEN app diff --local $INPUT_PATH $INPUT_APP
+argocd --server $INPUT_SERVER --auth-token "$INPUT_TOKEN" app diff --local $INPUT_PATH $INPUT_APP
