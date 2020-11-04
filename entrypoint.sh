513735bbc0f877c3aa1040bbee968817728cd6d9
Daniel Beal <dbeal@wiser.com>
2020-11-04 12:13:15 -0800

    check argocd

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+curl -Lo /usr/bin/argocd https://github.com/argoproj/argo-cd/releases/download/v1.7.8/argocd-linux-amd64
+chmod +x /usr/bin/argocd
+
+# TODO: latesr comment directly on the build and block it if fail
+argocd --server $INPUT_SERVER --auth-token $INPUT_TOKEN app diff --local $INPUT_PATH $INPUT_APP
