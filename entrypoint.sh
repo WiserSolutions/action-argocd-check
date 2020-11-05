7f6d70634c71b0a2f8341663c7559db4e90c1527
Daniel Beal <dbeal@wiser.com>
2020-11-05 10:58:18 -0800

    figure out certificate later

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,6 @@
 #chmod +x /usr/bin/argocd
 
 # TODO: latesr comment directly on the build and block it if fail
-argocd --server $INPUT_SERVER --auth-token "$INPUT_TOKEN" app diff --local $INPUT_PATH $INPUT_APP
+argocd --insecure --server $INPUT_SERVER --auth-token "$INPUT_TOKEN" app diff --local $INPUT_PATH $INPUT_APP
 
 echo $?
