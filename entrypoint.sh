c69bf15e3f7bdf41298a664b9692aae51bdc59bc
Daniel Beal <dbeal@wiser.com>
2020-11-05 10:56:33 -0800

    figure out what is going on with exit codes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 
-set -e
-set -x
+#set -e
+#set -x
 
 #curl -Lo /usr/bin/argocd https://github.com/argoproj/argo-cd/releases/download/v1.7.8/argocd-linux-amd64
 #chmod +x /usr/bin/argocd
 
 # TODO: latesr comment directly on the build and block it if fail
-argocd --insecure --server $INPUT_SERVER --auth-token "$INPUT_TOKEN" app diff --local $INPUT_PATH $INPUT_APP
+argocd --server $INPUT_SERVER --auth-token "$INPUT_TOKEN" app diff --local $INPUT_PATH $INPUT_APP
+
+echo $?
