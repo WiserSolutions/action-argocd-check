6e022c8dc42d827ecdef9c4c5b45e8f37acc44da
Daniel Beal <dbeal@wiser.com>
2020-11-05 10:52:31 -0800

    oops

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@
 set -e
 set -x
 
-curl -Lo /usr/bin/argocd https://github.com/argoproj/argo-cd/releases/download/v1.7.8/argocd-linux-amd64
-chmod +x /usr/bin/argocd
+#curl -Lo /usr/bin/argocd https://github.com/argoproj/argo-cd/releases/download/v1.7.8/argocd-linux-amd64
+#chmod +x /usr/bin/argocd
 
 # TODO: latesr comment directly on the build and block it if fail
 argocd --insecure --server $INPUT_SERVER --auth-token "$INPUT_TOKEN" app diff --local $INPUT_PATH $INPUT_APP
