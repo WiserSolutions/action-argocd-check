8a7bbf073f74ecf87ca1f8b929dec188ac5ff191
Daniel Beal <dbeal@wiser.com>
2020-11-05 15:52:00 -0800

    cleanup and handle errors even better

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -x
+#set -x
 
 # TODO: latesr comment directly on the build and block it if fail
 df=$(argocd --insecure --server $INPUT_SERVER --auth-token "$INPUT_TOKEN" app diff --local $INPUT_PATH $INPUT_APP 2>&1)
@@ -8,13 +8,24 @@ export res=$?
 
 echo "Result: $res"
 
-if [ $res -gt 0 ]; then
-  echo "presenting diff"
-  echo "$df"
+export pull_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 
-  export pull_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
+if [ $res -eq 20 ]; then
+  echo "argocd failed $df"
 
   curl -XPOST -H "Authorization: Bearer $INPUT_GITHUB_TOKEN" \
     $GITHUB_API_URL/repos/$GITHUB_REPOSITORY/issues/$pull_number/comments --data \
-    "$(jq --arg d "$df" -n '{ body: ("ArgoCD Diff\n```diff\n" + $d + "\n```") }')"
+    "$(jq --arg d "$df" -n '{ body: ("*ArgoCD Failure*\n```\n" + $d + "\n```") }')"
+
+  # exit with error to prevent merging
+  exit 1
+fi
+
+if [ $res -eq 1 ]; then
+  echo "presenting diff"
+  echo "$df"
+
+  curl -XPOST -H "Authorization: Bearer $INPUT_GITHUB_TOKEN" \
+    $GITHUB_API_URL/repos/$GITHUB_REPOSITORY/issues/$pull_number/comments --data \
+    "$(jq --arg d "$df" -n '{ body: ("*ArgoCD Diff*\n```diff\n" + $d + "\n```") }')"
 fi
