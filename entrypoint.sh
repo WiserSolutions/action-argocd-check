aed767dcd43ca44e3c466bd884ad9be7ddb73109
Daniel Beal <dbeal@wiser.com>
2020-11-05 14:35:03 -0800

    fix input bearer token

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-
-
-#set -e
-set -x
-
-env
+#set -x
 
 # TODO: latesr comment directly on the build and block it if fail
 df=$(argocd --insecure --server $INPUT_SERVER --auth-token "$INPUT_TOKEN" app diff --local $INPUT_PATH $INPUT_APP)
@@ -13,7 +8,6 @@ if [ $? -gt 0 ]; then
   echo "presenting diff"
   echo "$df"
 
-  pull_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
-
-  curl -XPOST -H "Authorization: Bearer $INPUT_GITHUB_TOKEN" $GITHUB_API_URL/repos/$GITHUB_REPOSITORY/issues/$pull_number/comments --data "{\"body\": \"ArgoCD Diff: \n\`\`\`diff\n$df\n\`\`\`\"}"
+  export pull_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
+  curl -XPOST -H "Authorization: Bearer ${INPUT_GITHUB-TOKEN}" $GITHUB_API_URL/repos/$GITHUB_REPOSITORY/issues/$pull_number/comments --data "{\"body\": \"ArgoCD Diff: \n\`\`\`diff\n$df\n\`\`\`\"}"
 fi
