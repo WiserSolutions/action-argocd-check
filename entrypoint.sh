d6ddce9db26e4d02ddcddba0afeeeae35641670b
Daniel Beal <dbeal@wiser.com>
2020-11-05 14:51:45 -0800

    do it

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#set -x
+set -x
 
 # TODO: latesr comment directly on the build and block it if fail
 df=$(argocd --insecure --server $INPUT_SERVER --auth-token "$INPUT_TOKEN" app diff --local $INPUT_PATH $INPUT_APP)
@@ -10,6 +10,5 @@ if [ $? -gt 0 ]; then
 
   export pull_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 
-  set -e
-  curl -XPOST -H "Authorization: Bearer ${INPUT_GITHUB_TOKEN}" $GITHUB_API_URL/repos/$GITHUB_REPOSITORY/issues/$pull_number/comments --data "{\"body\": \"ArgoCD Diff: \n\`\`\`diff\n$df\n\`\`\`\"}"
+  curl -XPOST -H "Authorization: Bearer ${INPUT_GITHUB_TOKEN}" $GITHUB_API_URL/repos/$GITHUB_REPOSITORY/issues/$pull_number/comments --data "${jq --arg d "$df" -n '{ body: "ArgoCD Diff\n```diff" + $d + "\n```" }'}"
 fi
